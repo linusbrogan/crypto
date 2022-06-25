6e9eb62d14a09b30da71f418b9a1f8eb5b87f755
Linus Brogan <git@linusbrogan.com>
2022-06-24 21:49:45 -0700

    Implement HOTP (RFC 4226)

---
+++ src/dev/brogan/otp/HOTP.java
@@ -0,0 +1,134 @@
+package dev.brogan.otp;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+/** An implementation of [RFC 4226](https://datatracker.ietf.org/doc/html/rfc4226) */
+public class HOTP {
+	// "Implementations MUST extract a 6-digit code at a minimum" (page 7).
+	private static final int DEFAULT_DIGITS = 6;
+	private static final int MINIMUM_DIGITS = 6;
+	// Truncating to 31 bits yields at most floor(31 * ln(10) / ln(2)) = 9 digits.
+	private static final int MAXIMUM_DIGITS = 9;
+	// "The length of the shared secret MUST be at least 128 bits" (page 5).
+	private static final int MINIMUM_SECRET_BYTES = 128 / 8;
+	private static final char[] HEX_CHARS = "0123456789abcdef".toCharArray();
+
+
+	/**
+	 * Generates a 6-digit HOTP value.
+	 * @param K "shared secret between client and server" (page 5).
+	 * @param C "8-byte counter value, the moving factor" (page 5).
+	 */
+	public static int HOTP(String K, long C) {
+		return HOTP(K, C, DEFAULT_DIGITS);
+	}
+
+	/**
+	 * Generates an HOTP value.
+	 * @param K "shared secret between client and server" (page 5).
+	 * @param C "8-byte counter value, the moving factor" (page 5).
+	 * @param Digit "number of digits in an HOTP value" (page 6).
+	 */
+	public static int HOTP(String K, long C, int Digit) {
+		// Check length requirements.
+		assert Digit >= MINIMUM_DIGITS;
+		assert Digit <= MAXIMUM_DIGITS;
+		assert K.getBytes().length >= MINIMUM_SECRET_BYTES;
+
+		return Truncate(HMAC_SHA1(K, C), Digit);
+	}
+
+	/**
+	 * @param K The HMAC key
+	 * @param C The 8-byte HMAC data
+	 */
+	static byte[] HMAC_SHA1(String K, long C) {
+		final String algorithm = "HmacSHA1";
+		final int nybblesPerLong = 2 * 8;
+		try {
+			Key key = new SecretKeySpec(K.getBytes(), algorithm);
+			Mac mac = Mac.getInstance(algorithm);
+			mac.init(key);
+			byte[] input = convertHexToBytes(zeroPad(Long.toHexString(C), nybblesPerLong));
+			return mac.doFinal(input);
+		} catch (NoSuchAlgorithmException | InvalidKeyException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/** Left-pads string with zeroes to at least targetLength. */
+	static String zeroPad(String string, int targetLength) {
+		int zeroes = targetLength - string.length();
+		if (targetLength < string.length()) zeroes = 0;
+		return "0".repeat(zeroes) + string;
+	}
+
+	/**
+	 * "Converts an HMAC-SHA-1 value into an HOTP value" (page 6).
+	 * @param hmac
+	 * @param Digit "number of digits in an HOTP value" (page 6).
+	 */
+	static int Truncate(byte[] hmac, int Digit) {
+		final int Snum = DT(hmac);
+		final int D = Snum % (int) Math.pow(10, Digit);
+		return D;
+	}
+
+	/**
+	 * Dynamic Truncation
+	 * Extracts 31 bits of the HMAC.
+	 * @param string a 20-byte HMAC
+	 */
+	static int DT(byte[] string) {
+		byte Offset = (byte) (string[19] & 0xf);
+		byte[] P = Arrays.copyOfRange(string, Offset, Offset + 4);
+		int dynamicBinaryCode = (P[0] & 0x7f) << 24 | (P[1] & 0xff) << 16 | (P[2] & 0xff) << 8 | (P[3] & 0xff);
+		return dynamicBinaryCode;
+	}
+
+	/**
+	 * @param hex an even-length string of hexadecimal digits
+	 */
+	static byte[] convertHexToBytes(String hex) {
+		int length = hex.length();
+		assert length % 1 != 1;
+		byte[] bytes = new byte[length / 2];
+		for (int i = 0; i < length; i += 2) {
+			char high = hex.charAt(i);
+			char low = hex.charAt(i + 1);
+			bytes[i / 2] = (byte) ((hexCharToByte(high) << 4) + hexCharToByte(low));
+		}
+		return bytes;
+	}
+
+	/**
+	 * @param c a hexadecimal digit
+	 */
+	static byte hexCharToByte(char c) {
+		if (c >= '0' && c <= '9') return (byte) (c - '0');
+		if (c >= 'a' && c <= 'f') return (byte) (c - 'a' + 10);
+		if (c >= 'A' && c <= 'F') return (byte) (c - 'A' + 10);
+
+		// A hexadecimal digit would have been handled already, so fail.
+		assert false;
+		return 0;
+	}
+
+	static String convertBytesToHex(byte[] bytes) {
+		StringBuilder nybbles = new StringBuilder();
+		for (byte b : bytes) {
+			int high = (byte) (b >>> 4) & 0xf;
+			int low = b & 0xf;
+
+			nybbles.append(HEX_CHARS[high]);
+			nybbles.append(HEX_CHARS[low]);
+		}
+		return nybbles.toString();
+	}
+}
---
+++ test/dev/brogan/otp/HOTPTest.java
@@ -0,0 +1,140 @@
+package dev.brogan.otp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HOTPTest {
+	private static final String secretASCII = "12345678901234567890";
+
+	private static final String[] intermediateHMACValues = {
+		"cc93cf18508d94934c64b65d8ba7667fb7cde4b0",
+		"75a48a19d4cbe100644e8ac1397eea747a2d33ab",
+		"0bacb7fa082fef30782211938bc1c5e70416ff44",
+		"66c28227d03a2d5529262ff016a1e6ef76557ece",
+		"a904c900a64b35909874b33e61c5938a8e15ed1c",
+		"a37e783d7b7233c083d4f62926c7a25f238d0316",
+		"bc9cd28561042c83f219324d3c607256c03272ae",
+		"a4fb960c0bc06e1eabb804e5b397cdc4b45596fa",
+		"1b3c89f65e6c9e883012052823443f048b4332db",
+		"1637409809a679dc698207310c8c7fc07290d9e5"
+	};
+
+	private static final int[] truncatedValues = {
+		0x4c93cf18,
+		0x41397eea,
+		0x82fef30,
+		0x66ef7655,
+		0x61c5938a,
+		0x33c083d4,
+		0x7256c032,
+		0x4e5b397,
+		0x2823443f,
+		0x2679dc69
+	};
+
+	private static final int[] HOTPValues = {
+		755224,
+		287082,
+		359152,
+		969429,
+		338314,
+		254676,
+		287922,
+		162583,
+		399871,
+		520489
+	};
+
+	private static final String HEX = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+	private static final String HEX_CHARS = "0123456789abcdef";
+
+	@Test
+	void generatesCorrectHOTPValues() {
+		for (int i = 0; i < HOTPValues.length; i++) {
+			int hotp = HOTP.HOTP(secretASCII, i);
+			assertEquals(HOTPValues[i], hotp);
+		}
+	}
+
+	@Test
+	void generatesCorrectIntermediateHMACValues() {
+		for (int i = 0; i < intermediateHMACValues.length; i++) {
+			byte[] hmac = HOTP.HMAC_SHA1(secretASCII, i);
+			assertEquals(intermediateHMACValues[i], HOTP.convertBytesToHex(hmac));
+		}
+	}
+
+	@Test
+	void generates8DigitHOTP() {
+		int i = 7;
+		int digits = 8;
+		assertEquals(truncatedValues[i], HOTP.HOTP(secretASCII, i, digits));
+	}
+
+	@Test
+	void failsWithShortDigits() {
+		int digits = 5;
+		assertThrows(Throwable.class, () -> HOTP.HOTP(secretASCII, 0, digits));
+	}
+
+	@Test
+	void failsWithLongDigits() {
+		int digits = 10;
+		assertThrows(Throwable.class, () -> HOTP.HOTP(secretASCII, 0, digits));
+	}
+
+	@Test
+	void failsWithShortSecret() {
+		String secret = "hello";
+		assertThrows(Throwable.class, () -> HOTP.HOTP(secret, 0));
+	}
+
+	@Test
+	void truncates() {
+		int digits = 6;
+		for (int i = 0; i < HOTPValues.length; i++) {
+			assertEquals(HOTPValues[i], HOTP.Truncate(HOTP.convertHexToBytes(intermediateHMACValues[i]), digits));
+		}
+	}
+
+	@Test
+	void dynamicallyTruncates() {
+		for (int i = 0; i < truncatedValues.length; i++) {
+			assertEquals(truncatedValues[i], HOTP.DT(HOTP.convertHexToBytes(intermediateHMACValues[i])));
+		}
+	}
+
+	@Test
+	void convertsHexToBytes() {
+		byte[] bytes = {1, 35, 69, 103, -119, -85, -51, -17};
+		assertArrayEquals(bytes, HOTP.convertHexToBytes(HEX_CHARS));
+		assertArrayEquals(bytes, HOTP.convertHexToBytes(HEX_CHARS.toUpperCase()));
+	}
+
+	@Test
+	void convertsHexCharToByte() {
+		char[] chars = HEX_CHARS.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			assertEquals(i, HOTP.hexCharToByte(chars[i]));
+		}
+
+		chars = HEX_CHARS.toUpperCase().toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			assertEquals(i, HOTP.hexCharToByte(chars[i]));
+		}
+	}
+
+	@Test
+	void convertsBytesToHex() {
+		byte[] bytes = {0x00, 0x47, 0x3f, 0x1a};
+		String hex = "00473f1a";
+		assertEquals(hex, HOTP.convertBytesToHex(bytes));
+	}
+
+	@Test
+	void zeroPads() {
+		assertEquals("2345", HOTP.zeroPad("2345", 2));
+		assertEquals("002345", HOTP.zeroPad("2345", 6));
+	}
+}
