2ffd366abbfdb93aa0b28a71d054f7684ade78e9
Linus Brogan <git@linusbrogan.com>
2022-08-16 15:21:45 -0700

    Implement AES (FIPS 197)

---
+++ src/dev/brogan/crypto/AES.java
@@ -0,0 +1,342 @@
+package dev.brogan.crypto;
+
+import java.util.Arrays;
+
+/** An implementation of [FIPS 197](https://doi.org/10.6028/NIST.FIPS.197) */
+public enum AES {
+	// Supported Key-Round combinations
+	AES128(128, 10),
+	AES192(192, 12),
+	AES256(256, 14);
+
+	private static final int BYTE_MASK = 0xff;
+	/** Word: "A group of 32 bits that is treated either as a single entity or as an array of 4 bytes" (page 6). */
+	private static final int WORD_SIZE = 4;
+	/** "Number of columns (32-bit words) comprising the State" (page 7). */
+	private static final int Nb = 4;
+	/**
+	 * "Non-linear substitution table used in several byte substitution transformations and in the Key Expansion routine to perform a one-for-one substitution of a byte value" (page 6).
+	 * Specified in Sec. 5.1.1.
+	 */
+	private static final byte[] S_BOX = Bytes.convertHexToBytes("637c777bf26b6fc53001672bfed7ab76ca82c97dfa5947f0add4a2af9ca472c0b7fd9326363ff7cc34a5e5f171d8311504c723c31896059a071280e2eb27b27509832c1a1b6e5aa0523bd6b329e32f8453d100ed20fcb15b6acbbe394a4c58cfd0efaafb434d338545f9027f503c9fa851a3408f929d38f5bcb6da2110fff3d2cd0c13ec5f974417c4a77e3d645d197360814fdc222a908846eeb814de5e0bdbe0323a0a4906245cc2d3ac629195e479e7c8376d8dd54ea96c56f4ea657aae08ba78252e1ca6b4c6e8dd741f4bbd8b8a703eb5664803f60e613557b986c11d9ee1f8981169d98e949b1e87e9ce5528df8ca1890dbfe6426841992d0fb054bb16");
+	/** Inverse S-box (page 22). */
+	private static final byte[] INV_S_BOX = Bytes.convertHexToBytes("52096ad53036a538bf40a39e81f3d7fb7ce339829b2fff87348e4344c4dee9cb547b9432a6c2233dee4c950b42fac34e082ea16628d924b2765ba2496d8bd12572f8f66486689816d4a45ccc5d65b6926c704850fdedb9da5e154657a78d9d8490d8ab008cbcd30af7e45805b8b34506d02c1e8fca3f0f02c1afbd0301138a6b3a9111414f67dcea97f2cfcef0b4e67396ac7422e7ad3585e2f937e81c75df6e47f11a711d29c5896fb7620eaa18be1bfc563e4bc6d279209adbc0fe78cd5af41fdda8338807c731b11210592780ec5f60517fa919b54a0d2de57a9f93c99cefa0e03b4dae2af5b0c8ebbb3c83539961172b047eba77d626e169146355210c7d");
+
+	/** "The round constant word array" (page 7). */
+	private final byte[][] Rcon = new byte[11][WORD_SIZE];
+	{
+		byte x = 1;
+		for (int i = 1; i < Rcon.length; i++) {
+			Rcon[i][0] = x;
+			x = xtime(x);
+		}
+	}
+
+	/** "Number of 32-bit words comprising the Cipher Key" (page 7). */
+	private final int Nk;
+	/** "Number of rounds, which is a function of Nk and Nb (which is fixed)" (page 7). */
+	private final int Nr;
+
+	AES(int keyLengthBits, int Nr) {
+		this.Nk = keyLengthBits / (8 * WORD_SIZE);
+		this.Nr = Nr;
+	}
+
+	/** Multiplication in GF(2^8) (Sec. 4.2). */
+	static byte mult(byte a, byte b) {
+		byte product = 0;
+		byte leftTerm = a;
+		for (int i = 0; i < 8; i++) {
+			byte bit = (byte) (b & 1);
+			product ^= bit * leftTerm;
+			leftTerm = xtime(leftTerm);
+			b >>= 1;
+		}
+		return product;
+	}
+
+	/** Multiplication by x = {02} in GF(2^8) (Sec. 4.2.1). */
+	static byte xtime(byte a) {
+		boolean isHighBitSet = a < 0;
+		a <<= 1;
+		if (isHighBitSet) {
+			a ^= 0x1b;
+		}
+		return a;
+	}
+
+	/**
+	 * "Series of transformations that converts plaintext to ciphertext using the Cipher Key" (page 6).
+	 * Specified in Sec. 5.1.
+	 * @param in message block to encrypt
+	 * @param w key schedule
+	 * @return ciphertext block
+	 */
+	byte[] Cipher(byte[] in, byte[][] w) {
+		assert in.length == WORD_SIZE * Nb;
+		assert w.length == Nb * (Nr + 1);
+		byte[][] state = new byte[WORD_SIZE][Nb];
+		for (int r = 0; r < WORD_SIZE; r++) {
+			for (int c = 0; c < Nb; c++) {
+				state[r][c] = in[r + WORD_SIZE * c];
+			}
+		}
+		AddRoundKey(state, Arrays.copyOfRange(w, 0, Nb - 1 + 1));
+		for (int round = 1; round <= Nr - 1; round++) {
+			SubBytes(state);
+			ShiftRows(state);
+			MixColumns(state);
+			AddRoundKey(state, Arrays.copyOfRange(w, round * Nb, (round + 1) * Nb - 1 + 1));
+		}
+		SubBytes(state);
+		ShiftRows(state);
+		AddRoundKey(state, Arrays.copyOfRange(w, Nr * Nb, (Nr + 1) * Nb - 1 + 1));
+
+		byte[] out = new byte[in.length];
+		for (int r = 0; r < WORD_SIZE; r++) {
+			for (int c = 0; c < Nb; c++) {
+				out[r + WORD_SIZE * c] = state[r][c];
+			}
+		}
+		return out;
+	}
+
+	/**
+	 * "Transformation in the Cipher that processes the State using a nonlinear byte substitution table (S-box) that operates on each of the State bytes independently" (page 7).
+	 * Specified in Sec. 5.1.1.
+	 * */
+	static void SubBytes(byte[][] state) {
+		for (int r = 0; r < WORD_SIZE; r++) {
+			for (int c = 0; c < Nb; c++) {
+				state[r][c] = S_BOX[state[r][c] & BYTE_MASK];
+			}
+		}
+	}
+
+	/**
+	 * "Transformation in the Cipher that processes the State by cyclically shifting the last three rows of the State by different offsets" (page 7).
+	 * Specified in Sec. 5.1.2.
+	 */
+	static void ShiftRows(byte[][] state) {
+		for (int r = 1; r < state.length; r++) {
+			byte[] newRow = new byte[state[r].length];
+			for (int c = 0; c < Nb; c++) {
+				newRow[c] = state[r][(c + r) % Nb];
+			}
+			state[r] = newRow;
+		}
+	}
+
+	/**
+	 * "Transformation in the Cipher that takes all of the columns of the State and mixes their data (independently of one another) to produce new columns" (page 7).
+	 * Specified in Sec. 5.1.3.
+	 */
+	static void MixColumns(byte[][] state) {
+		for (int c = 0; c < Nb; c++) {
+			byte[] newColumn = new byte[WORD_SIZE];
+			newColumn[0] = (byte) (mult((byte) 2, state[0][c]) ^ mult((byte) 3, state[1][c]) ^ state[2][c] ^ state[3][c]);
+			newColumn[1] = (byte) (state[0][c] ^ mult((byte) 2, state[1][c]) ^ mult((byte) 3, state[2][c]) ^ state[3][c]);
+			newColumn[2] = (byte) (state[0][c] ^ state[1][c] ^ mult((byte) 2, state[2][c]) ^ mult((byte) 3, state[3][c]));
+			newColumn[3] = (byte) (mult((byte) 3, state[0][c]) ^ state[1][c] ^ state[2][c] ^ mult((byte) 2, state[3][c]));
+			for (int r = 0; r < newColumn.length; r++) {
+				state[r][c] = newColumn[r];
+			}
+		}
+	}
+
+	/**
+	 * "Transformation in the Cipher and Inverse Cipher in which a Round Key is added to the State using an XOR operation" (page 6).
+	 * Specified in Sec. 5.1.4.
+	 */
+	static void AddRoundKey(byte[][] state, byte[][] w) {
+		for (int c = 0; c < Nb; c++) {
+			for (int r = 0; r < WORD_SIZE; r++) {
+				state[r][c] ^= w[c][r];
+			}
+		}
+	}
+
+	/**
+	 * "Function used in the Key Expansion routine that takes a four-byte input word and applies an S-box to each of the four bytes to produce an output word" (page 7).
+	 * Specified in Sec. 5.2.
+	 */
+	static byte[] SubWord(byte[] word) {
+		byte[] subWord = new byte[word.length];
+		for (int i = 0 ; i < word.length; i++) {
+			subWord[i] = S_BOX[word[i] & BYTE_MASK];
+		}
+		return subWord;
+	}
+
+	/**
+	 * "Function used in the Key Expansion routine that takes a four-byte word and performs a cyclic permutation" (page 7).
+	 * Specified in Sec. 5.2.
+	 */
+	static byte[] RotWord(byte[] word) {
+		byte[] rotWord = new byte[word.length];
+		for (int i = 0; i < word.length; i++) {
+			rotWord[i] = word[(i + 1) % word.length];
+		}
+		return rotWord;
+	}
+
+	/**
+	 * "Routine used to generate a series of Round Keys from the Cipher Key" (page 6).
+	 * Specified in Sec. 5.2.
+	 */
+	byte[][] KeyExpansion(byte[] key) {
+		assert key.length == WORD_SIZE * Nk;
+
+		byte[][] w = new byte[Nb * (Nr + 1)][WORD_SIZE];
+
+		for (int i = 0; i < Nk; i++) {
+			for (int j = 0; j < w[i].length; j++) {
+				w[i][j] = key[4 * i + j];
+			}
+		}
+
+		for (int i = Nk; i < w.length; i++) {
+			byte[] temp = w[i - 1];
+			if (i % Nk == 0) {
+				temp = Bytes.xor(SubWord(RotWord(temp)), Rcon[i / Nk]);
+			} else if (Nk > 6 && i % Nk == 4) {
+				temp = SubWord(temp);
+			}
+			w[i] = Bytes.xor(w[i - Nk], temp);
+		}
+
+		return w;
+	}
+
+	/**
+	 * Inverse Cipher: "Series of transformations that converts ciphertext to plaintext using the Cipher Key" (page 6).
+	 * Specified in Sec. 5.3.
+	 * @param in ciphertext block to decrypt
+	 * @param w key schedule
+	 * @return message block
+	 */
+	byte[] InvCipher(byte[] in, byte[][] w) {
+		assert in.length == WORD_SIZE * Nb;
+		assert w.length == Nb * (Nr + 1);
+		byte[][] state = new byte[WORD_SIZE][Nb];
+		for (int r = 0; r < WORD_SIZE; r++) {
+			for (int c = 0; c < Nb; c++) {
+				state[r][c] = in[r + WORD_SIZE * c];
+			}
+		}
+		AddRoundKey(state, Arrays.copyOfRange(w, Nr * Nb, (Nr + 1) * Nb - 1 + 1));
+		for (int round = Nr - 1; round >= 1; round--) {
+			InvShiftRows(state);
+			InvSubBytes(state);
+			AddRoundKey(state, Arrays.copyOfRange(w, round * Nb, (round + 1) * Nb - 1 + 1));
+			InvMixColumns(state);
+		}
+		InvShiftRows(state);
+		InvSubBytes(state);
+		AddRoundKey(state, Arrays.copyOfRange(w, 0, Nb - 1 + 1));
+
+		byte[] out = new byte[in.length];
+		for (int r = 0; r < WORD_SIZE; r++) {
+			for (int c = 0; c < Nb; c++) {
+				out[r + WORD_SIZE * c] = state[r][c];
+			}
+		}
+		return out;
+	}
+
+	/**
+	 * "Transformation in the Inverse Cipher that is the inverse of ShiftRows()" (page 6).
+	 * Specified in Sec. 5.3.1.
+	 */
+	static void InvShiftRows(byte[][] state) {
+		for (int r = 1; r < state.length; r++) {
+			byte[] newRow = new byte[state[r].length];
+			for (int c = 0; c < Nb; c++) {
+				newRow[(c + r) % Nb] = state[r][c];
+			}
+			state[r] = newRow;
+		}
+	}
+
+	/**
+	 * "Transformation in the Inverse Cipher that is the inverse of SubBytes()" (page 6).
+	 * Specified in Sec. 5.3.2.
+	 */
+	static void InvSubBytes(byte[][] state) {
+		for (int r = 0; r < WORD_SIZE; r++) {
+			for (int c = 0; c < Nb; c++) {
+				state[r][c] = INV_S_BOX[state[r][c] & BYTE_MASK];
+			}
+		}
+	}
+
+	/**
+	 * "Transformation in the Inverse Cipher that is the inverse of MixColumns()" (page 6).
+	 * Specified in Sec. 5.3.3.
+	 */
+	static void InvMixColumns(byte[][] state) {
+		for (int c = 0; c < Nb; c++) {
+			byte[] newColumn = new byte[WORD_SIZE];
+			newColumn[0] = (byte) (mult((byte) 0xe, state[0][c]) ^ mult((byte) 0xb, state[1][c]) ^ mult((byte) 0xd, state[2][c]) ^ mult((byte) 0x9, state[3][c]));
+			newColumn[1] = (byte) (mult((byte) 0x9, state[0][c]) ^ mult((byte) 0xe, state[1][c]) ^ mult((byte) 0xb, state[2][c]) ^ mult((byte) 0xd, state[3][c]));
+			newColumn[2] = (byte) (mult((byte) 0xd, state[0][c]) ^ mult((byte) 0x9, state[1][c]) ^ mult((byte) 0xe, state[2][c]) ^ mult((byte) 0xb, state[3][c]));
+			newColumn[3] = (byte) (mult((byte) 0xb, state[0][c]) ^ mult((byte) 0xd, state[1][c]) ^ mult((byte) 0x9, state[2][c]) ^ mult((byte) 0xe, state[3][c]));
+			for (int r = 0; r < newColumn.length; r++) {
+				state[r][c] = newColumn[r];
+			}
+		}
+	}
+
+	/**
+	 * Equivalent Inverse Cipher (Sec 5.3.5).
+	 * @param in ciphertext block to decrypt
+	 * @param dw modified decryption key schedule
+	 * @return message block
+	 */
+	byte[] EqInvCipher(byte[] in, byte[][] dw) {
+		assert in.length == WORD_SIZE * Nb;
+		assert dw.length == Nb * (Nr + 1);
+		byte[][] state = new byte[WORD_SIZE][Nb];
+		for (int r = 0; r < WORD_SIZE; r++) {
+			for (int c = 0; c < Nb; c++) {
+				state[r][c] = in[r + WORD_SIZE * c];
+			}
+		}
+		AddRoundKey(state, Arrays.copyOfRange(dw, Nr * Nb,  (Nr + 1) * Nb - 1 + 1));
+		for (int round = Nr - 1; round >= 1; round--) {
+			InvSubBytes(state);
+			InvShiftRows(state);
+			InvMixColumns(state);
+			AddRoundKey(state, Arrays.copyOfRange(dw, round * Nb, (round + 1) * Nb - 1 + 1));
+		}
+		InvSubBytes(state);
+		InvShiftRows(state);
+		AddRoundKey(state, Arrays.copyOfRange(dw, 0, Nb - 1 + 1));
+
+		byte[] out = new byte[in.length];
+		for (int r = 0; r < WORD_SIZE; r++) {
+			for (int c = 0; c < Nb; c++) {
+				out[r + WORD_SIZE * c] = state[r][c];
+			}
+		}
+		return out;
+	}
+
+	/** Modified Key Expansion routine (Sec 5.3.5). */
+	byte[][] ModifiedKeyExpansion(byte[][] w) {
+		byte[][] dw = new byte[Nb * (Nr + 1)][WORD_SIZE];
+		for (int i = 0; i <= (Nr + 1) * Nb - 1; i++) {
+			dw[i] = w[i];
+		}
+		for (int round = 1; round <= Nr - 1; round++) {
+			byte[][] roundKey = Arrays.copyOfRange(dw, round * Nb, (round + 1) * Nb - 1 + 1);
+			roundKey = Bytes.transpose(roundKey);
+			InvMixColumns(roundKey);
+			roundKey = Bytes.transpose(roundKey);
+			for (int i = 0; i < Nb; i++) {
+				dw[round * Nb + i] = roundKey[i];
+			}
+		}
+		return dw;
+	}
+}
---
+++ test/dev/brogan/crypto/AESTest.java
@@ -0,0 +1,165 @@
+package dev.brogan.crypto;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AESTest {
+	// Key Expansion Examples (App. A)
+	// Expansion of a 128-bit Cipher Key (A.1)
+	private static final byte[] CIPHER_KEY_128b = Bytes.convertHexToBytes("2b7e151628aed2a6abf7158809cf4f3c");
+	private static final byte[][] w_128b = convertHexToWordArray("2b7e151628aed2a6abf7158809cf4f3ca0fafe1788542cb123a339392a6c7605f2c295f27a96b9435935807a7359f67f3d80477d4716fe3e1e237e446d7a883bef44a541a8525b7fb671253bdb0bad00d4d1c6f87c839d87caf2b8bc11f915bc6d88a37a110b3efddbf98641ca0093fd4e54f70e5f5fc9f384a64fb24ea6dc4fead27321b58dbad2312bf5607f8d292fac7766f319fadc2128d12941575c006ed014f9a8c9ee2589e13f0cc8b6630ca6");
+	// Expansion of a 192-bit Cipher Key (A.2)
+	private static final byte[] CIPHER_KEY_192b = Bytes.convertHexToBytes("8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b");
+	private static final byte[][] w_192b = convertHexToWordArray("8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7bfe0c91f72402f5a5ec12068e6c827f6b0e7a95b95c56fec24db7b4bd69b5411885a74796e92538fde75fad44bb095386485af05721efb14fa448f6d94d6dce24aa326360113b30e6a25e7ed583b1cf9a27f939436a94f767c0a69407d19da4e1ec1786eb6fa64971485f703222cb8755e26d135233f0b7b340beeb282f18a2596747d26b458c553ea7e1466c9411f1df821f750aad07d753ca4005388fcc5006282d166abc3ce7b5e98ba06f448c773c8ecc720401002202");
+	// Expansion of a 256-bit Cipher Key (A.3)
+	private static final byte[] CIPHER_KEY_256b = Bytes.convertHexToBytes("603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4");
+	private static final byte[][] w_256b = convertHexToWordArray("603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff49ba354118e6925afa51a8b5f2067fcdea8b09c1a93d194cdbe49846eb75d5b9ad59aecb85bf3c917fee94248de8ebe96b5a9328a2678a647983122292f6c79b3812c81addadf48ba24360af2fab8b46498c5bfc9bebd198e268c3ba709e0421468007bacb2df331696e939e46c518d80c814e20476a9fb8a5025c02d59c58239de1369676ccc5a71fa2563959674ee155886ca5d2e2f31d77e0af1fa27cf73c3749c47ab18501ddae2757e4f7401905acafaaae3e4d59b349adf6acebd10190dfe4890d1e6188d0b046df344706c631e");
+
+	private static byte[][] convertHexToWordArray(String hex) {
+		int wordSize = 4;
+		byte[] bytes = Bytes.convertHexToBytes(hex);
+		byte[][] words = new byte[bytes.length / wordSize][wordSize];
+		for (int i = 0; i < bytes.length; i++) {
+			words[i / 4][i % 4] = bytes[i];
+		}
+		return words;
+	}
+
+	@Test
+	void expandsKeys() {
+		assertArrayEquals(w_128b, AES.AES128.KeyExpansion(CIPHER_KEY_128b));
+		assertArrayEquals(w_192b, AES.AES192.KeyExpansion(CIPHER_KEY_192b));
+		assertArrayEquals(w_256b, AES.AES256.KeyExpansion(CIPHER_KEY_256b));
+	}
+
+	@Test
+	void substitutesWordsPurely() {
+		byte[] before = {0x32, (byte) 0xab, (byte) 0x97, (byte) 0xa1};
+		byte[] beforeCopy = Arrays.copyOf(before, before.length);
+		byte[] after = {0x23, 0x62, (byte) 0x88, 0x32};
+		assertArrayEquals(after, AES.SubWord(before));
+		assertArrayEquals(beforeCopy, before);
+	}
+
+	@Test
+	void rotatesWordsPurely() {
+		byte[] before = {10, 14, -3, 6};
+		byte[] beforeCopy = Arrays.copyOf(before, before.length);
+		byte[] after = {14, -3, 6, 10};
+		assertArrayEquals(after, AES.RotWord(before));
+		assertArrayEquals(beforeCopy, before);
+	}
+
+	// Cipher Example (App. B)
+	byte[] CIPHER_INPUT = Bytes.convertHexToBytes("3243f6a8885a308d313198a2e0370734");
+	byte[] CIPHER_KEY = Bytes.convertHexToBytes("2b7e151628aed2a6abf7158809cf4f3c");
+	// Output block column-by-column (page 34)
+	byte[] CIPHER_OUTPUT = Bytes.convertHexToBytes("3925841d02dc09fbdc118597196a0b32");
+
+	@Test
+	void encrypts() {
+		assertArrayEquals(CIPHER_OUTPUT, AES.AES128.Cipher(CIPHER_INPUT, AES.AES128.KeyExpansion(CIPHER_KEY)));
+	}
+
+	// Intermediate State values (page 33)
+	String[] STATES = {
+		"19a09ae93df4c6f8e3e28d48be2b2a08", // Round 1, Start of Round
+		"d4e0b81e27bfb44111985d52aef1e530", // Round 1, After SubBytes
+		"d4e0b81ebfb441275d52119830aef1e5", // Round 1, After ShiftRows
+		"04e0482866cbf8068119d326e59a7a4c", // Round 1, After MixColumns
+		"a0fafe1788542cb123a339392a6c7605", // Round 1, Round Key Value
+		"a4686b029c9f5b6a7f35ea50f22b4349" // Round 2, Start of Round
+	};
+
+	@Test
+	void substitutesBytes() {
+		int step = 0;
+		byte[][] before = convertHexToWordArray(STATES[step]);
+		byte[][] after = convertHexToWordArray(STATES[step + 1]);
+		AES.SubBytes(before);
+		assertArrayEquals(after, before);
+	}
+
+	@Test
+	void shiftsRows() {
+		int step = 1;
+		byte[][] before = convertHexToWordArray(STATES[step]);
+		byte[][] after = convertHexToWordArray(STATES[step + 1]);
+		AES.ShiftRows(before);
+		assertArrayEquals(after, before);
+	}
+
+	@Test
+	void mixesColumns() {
+		int step = 2;
+		byte[][] before = convertHexToWordArray(STATES[step]);
+		byte[][] after = convertHexToWordArray(STATES[step + 1]);
+		AES.MixColumns(before);
+		assertArrayEquals(after, before);
+	}
+
+	@Test
+	void multipliesBytes() {
+		// Products from page 11
+		assertEquals((byte) 0xc1, AES.mult((byte) 0x57, (byte) 0x83));
+		assertEquals((byte) 0xfe, AES.mult((byte) 0x57, (byte) 0x13));
+	}
+
+	@Test
+	void multipliesByX() {
+		// Products from page 12
+		byte[] actual = {0x57, (byte) 0xae, 0x47, (byte) 0x8e};
+		for (int i = 0; i < actual.length; i++) {
+			actual[i] = AES.xtime(actual[i]);
+		}
+		byte[] expected = {(byte) 0xae, 0x47, (byte) 0x8e, 0x07};
+		assertArrayEquals(expected, actual);
+	}
+
+	@Test
+	void addsRoundKey() {
+		int step = 3;
+		byte[][] before = convertHexToWordArray(STATES[step]);
+		byte[][] roundKey = convertHexToWordArray(STATES[step + 1]);
+		byte[][] after = convertHexToWordArray(STATES[step + 2]);
+		AES.AddRoundKey(before, roundKey);
+		assertArrayEquals(after, before);
+	}
+
+	@Test
+	void decrypts() {
+		byte[][] w = AES.AES128.KeyExpansion(CIPHER_KEY);
+		assertArrayEquals(CIPHER_INPUT, AES.AES128.InvCipher(CIPHER_OUTPUT, w));
+		assertArrayEquals(CIPHER_INPUT, AES.AES128.EqInvCipher(CIPHER_OUTPUT, AES.AES128.ModifiedKeyExpansion(w)));
+	}
+
+	@Test
+	void inverseSubstitutesBytes() {
+		int step = 0;
+		byte[][] before = convertHexToWordArray(STATES[step + 1]);
+		byte[][] after = convertHexToWordArray(STATES[step]);
+		AES.InvSubBytes(before);
+		assertArrayEquals(after, before);
+	}
+
+	@Test
+	void inverseShiftsRows() {
+		int step = 1;
+		byte[][] before = convertHexToWordArray(STATES[step + 1]);
+		byte[][] after = convertHexToWordArray(STATES[step]);
+		AES.InvShiftRows(before);
+		assertArrayEquals(after, before);
+	}
+
+	@Test
+	void inverseMixesColumns() {
+		int step = 2;
+		byte[][] before = convertHexToWordArray(STATES[step + 1]);
+		byte[][] after = convertHexToWordArray(STATES[step]);
+		AES.InvMixColumns(before);
+		assertArrayEquals(after, before);
+	}
+}
